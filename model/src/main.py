2786f161e6d3128f0631b853a017a10a74eb577c
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-11-09 13:58:14 +0900

    feat: add .gitignore and requirements.txt; update main.py to load MNIST dataset

--- model/src/main.py
+++ model/src/main.py
@@ -1,6 +1,12 @@
+from tensorflow import keras
+
 
 def main():
     print("Hello World!")
+    print("Loading MNIST dataset...")
+    keras.datasets.mnist.load_data()
+    print(keras.__version__)
+
 
 if __name__ == "__main__":
     main()
