c347d843a00f10d51800a45d41eeb590656a3944
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-11-19 14:01:49 +0900

    Update main.py

--- model/src/main.py
+++ model/src/main.py
@@ -2,6 +2,7 @@ from train.sudokuNet import sudokuNet
 import os
 import datetime
 
+
 def main():
     sudokuSolverModel = sudokuNet()
     modelURL = '1112-135907-epochs10.keras'
@@ -17,6 +18,7 @@ def main():
         sudokuSolverModel.predict(folder_path + filename)
 
     # Plot the training history
+    print("/n The evvaluation of the model is:")
     sudokuSolverModel.evaluate()
 
 
@@ -28,10 +30,12 @@ def train(model):
     # Total time taken
     print(f"Total time taken: {datetime.datetime.now() - start}")
 
+
 if __name__ == "__main__":
     # check if running from model directory
-    if not(os.getcwd().split('/')[-1] == 'model'):
-        print('==' * 24, '\nPlease switch directory to model directory\n', '==' * 24, sep='')
+    if not (os.getcwd().split('/')[-1] == 'model'):
+        print('==' * 24, '\nPlease switch directory to model directory\n',
+              '==' * 24, sep='')
         exit()
 
     # run main function
